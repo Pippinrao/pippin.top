98b115dbe5919ed4243ddc32979e6bbb7f7e7874
pippin <bobinrao@seu.edu.cn>
2019-10-06 14:24:39 +0800

    change front end

--- note/models.py
+++ note/models.py
@@ -1,14 +1,45 @@
 from django.db import models
-import datetime
+from django.utils import timezone
 from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
 
 
 class Note(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=500)
     body = models.TextField()
-    created = models.DateTimeField(default=datetime.datetime.now())
+    created = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(auto_now=True)
+
+    category = models.ForeignKey(
+        Category,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='note'
+    )
+
+    tag = models.ManyToManyField(
+        Tag,
+        related_name='note',
+        null=True,
+        blank=True,
+    )
 
     class Meta:
         ordering = ('-updated',)
--- pippin/settings.py
+++ pippin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd1yaimr^yd4_#-qn9&#r^7s77$+!z3+j)5m0m@st@&y0m&kl%0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'note',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
